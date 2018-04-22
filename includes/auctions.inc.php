18c5c74287c624c88aef433e9a277492e93e74bc
Vlad Stanescu <vladstanescu94@gmail.com>
2018-04-22 18:09:00 +0300

    product add functionality

--- includes/auctions.inc.php
+++ includes/auctions.inc.php
@@ -2,8 +2,23 @@
 
 if(isset($_POST['submit'])){
     include_once 'dbh.inc.php';
+    $name  = mysqli_real_escape_string($conn, $_POST['product_name']);
+    $descr = mysqli_real_escape_string($conn, $_POST['product_descr']);
+    $image = $_POST['product_img'];
+    $cat   = $_POST['product_cat'];
 
-    
+    if (empty($name) || empty($cat)){
+        header("Location: ../auctions.php?prduct=empty");
+        exit();
+    } 
+    else {
+            $sql = "INSERT INTO products (product_name, product_description, product_image, product_category)
+                     VALUES ('$name', '$descr', '$image', '$cat');";
+            mysqli_query($conn, $sql); 
+            header("Location: ../auctions.php?product=success");
+            exit();
+    }   
+
 
 }else {
     header("Location: ../auctions.php");
