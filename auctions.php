41b292c66e9ccfa134977338548115507ab8e2e6
Vlad Stanescu <vladstanescu94@gmail.com>
2018-04-22 18:08:22 +0300

    added dynamic select options

--- auctions.php
+++ auctions.php
@@ -1,5 +1,6 @@
 <?php
     include_once 'header.php';
+    include_once '/includes/dbh.inc.php';
 ?>
     <section class="auctions">
         <div class="container">
@@ -9,12 +10,17 @@
                     <form action="includes/auctions.inc.php" class="auction-add__form" method="POST">
                         <input type="text" class="auction__input" name="product_name" placeholder="Product name">
                         <input type="text" class="auction__input" name="product_descr" placeholder="Product description">
-                        <input type="file" class="auction__input" name="product_img" placeholder="Product image">
-                        <select class="auction__select" name="product_cat">
-                            <option class="select__option" value="fineart">Fine Art</option>
-                            <option class="select__option" value="jewelry">Jewelry</option>
-                            <option class="select__option" value="collectibles">Collectibles</option>
-                        </select>
+                        <input type="file" class="auction__input" name="product_img">
+                        <select class="auction__select" name="product_cat">';
+                            $sql = "SELECT * FROM categories";
+                            $result = mysqli_query($conn, $sql);
+                             if ($result->num_rows > 0) {
+                                // output data of each row
+                                 while($row = $result->fetch_assoc()) {
+                                   echo '<option class="select__option" value='.$row["category_id"].'>'.$row["category_name"].'</option>';
+                                 }
+                             }   
+                echo   '</select>
                         <button type="submit" name="submit" class="btn btn--center">Add Product</button>
                     </form>';
             }
